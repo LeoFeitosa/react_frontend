63bfcd9683dee254317c28118ed150213fc015a5
Leonardo Feitosa <leonardo.feitosa@uniesp.edu.br>
2018-05-18 17:37:49 -0300

    adicionado chamada de funções pelo teclado

--- src/todo/todoForm.jsx
+++ src/todo/todoForm.jsx
@@ -2,14 +2,24 @@ import React from 'react'
 import Grid from '../template/grid'
 import IconButton from '../template/iconButton'
 
-export default props => (
-  <div role="form" className="todoForm">
+export default props => {
+  const keyHandler = (e) => {
+    if (e.key == 'Enter') {
+      e.shiftKey ? props.handleSearch() : handleAdd()
+    } else if(e.key == 'Escape') {
+      props.handleClear()
+    }
+  }
+
+  return (
+    <div role="form" className="todoForm">
 
     <Grid cols="12 9 10">
       <input id="description" className="form-control"
         placeholder="Adicione uma tarefa"
         value={props.description}
-        onChange={props.handleChange}/>
+        onChange={props.handleChange}
+        onKeyUp={keyHandler}/>
     </Grid>
 
     <Grid cols="12 3 2">
@@ -19,4 +29,5 @@ export default props => (
     </Grid>
 
   </div>
-)
+  )
+}
